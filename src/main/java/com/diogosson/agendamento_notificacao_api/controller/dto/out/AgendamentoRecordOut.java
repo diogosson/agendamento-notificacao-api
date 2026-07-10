a4fa9b1b526e18586a4bef06daa4656bc7f62ab7
Pitonico <dio_dgba@hotmail.com>
2026-07-10 18:47:28 -0300

    adicionado métodos de agendamento de notificação

---
+++ src/main/java/com/diogosson/agendamento_notificacao_api/controller/dto/out/AgendamentoRecordOut.java
@@ -0,0 +1,16 @@
+package com.diogosson.agendamento_notificacao_api.controller.dto.out;
+
+import com.diogosson.agendamento_notificacao_api.infrastructure.enums.StatusNotificacaoEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record AgendamentoRecordOut(
+        Long id,
+        String emailDestinatario,
+        String telefoneDestinatario,
+        String mensagem,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss") LocalDateTime dataHoraEnvio,
+        StatusNotificacaoEnum statusNotificacao
+) {
+}
